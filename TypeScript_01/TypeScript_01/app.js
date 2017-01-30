ba5f762a3e74b79a0797c5ed61c62e100e5cd3e1
Andras . <andras.varro@axelero.hu>
2017-01-30 14:43:48 +0100

    Try half JavaScript half TypeScript mixture

--- TypeScript_01/TypeScript_01/app.js
+++ TypeScript_01/TypeScript_01/app.js
@@ -1,24 +1,21 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-
-    constructor(element: HTMLElement) {
+var Greeter = (function () {
+    function Greeter(element) {
         this.element = element;
         this.element.innerHTML += "Bezabáltam: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
     }
-
-    start(food: string, energy: number) {
+    Greeter.prototype.start = function (food, energy) {
         this.span.innerHTML = "Our " + food + " has " + energy + " calories.";
-    }
-}
-var burger: string = 'hamburger',     // String 
-    calories: number = 300,           // Numeric
-    tasty: boolean = true;            // Boolean
-
-window.onload = () => {
+    };
+    return Greeter;
+}());
+var burger = 'hamburger', // String 
+calories = 300, // Numeric
+tasty = true; // Boolean
+window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start(burger, calories);
 };
+//# sourceMappingURL=app.js.map
