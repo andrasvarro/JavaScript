efc72c214a566b621f5e6f73bae9cf1cef6bbd52
Andras . <andras.varro@axelero.hu>
2017-01-31 09:55:35 +0100

    file read async

--- First/First/main.js
+++ First/First/main.js
@@ -1,4 +1,8 @@
-﻿var fs = require("fs");
-var data = fs.readFileSync('input.txt');
-console.log(data.toString());
+﻿// to run use node.js
+// node main.js
+var fs = require("fs");
+var data = fs.readFile('input.txt', function (err, data){
+    if (err) return console.error(err);
+    console.log(data.toString());
+})
 console.log("Program Ended");
