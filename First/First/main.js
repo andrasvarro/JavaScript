4744a3ab9fe315c614f695ed1d9532939c63ca60
Andras . <andras.varro@axelero.hu>
2017-01-31 09:52:19 +0100

    Sync file read node.js

--- First/First/main.js
+++ First/First/main.js
@@ -1,7 +1,4 @@
-﻿var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World!\n');
-}).listen(8081);
-
-console.log('Server running at http://localhost:8081/');
+﻿var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+console.log(data.toString());
+console.log("Program Ended");
